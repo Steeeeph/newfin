c8f51ff892e62875d7312d96ae01465cd05ed1ea
Steeeeph <85747699+Steeeeph@users.noreply.github.com>
2023-07-22 22:15:30 +0200

    Update index.ts
    change name

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,18 @@ window.Webflow.push(() => {
   const name = 'John Doe';
   greetUser(name);
 });
+$(document).ready(function () {
+  $('.menu_btn').click(function () {
+    $('body').toggleClass('overflow_hidden');
+  });
+});
+let menu_btn;
+menu_btn.addEventListener('click', function () {
+  if (menu_btn.classList.contains('open_icon')) {
+    menu_btn.classList.add('close_icon');
+    menu_btn.classList.remove('open_icon');
+  } else {
+    menu_btn.classList.remove('close_icon');
+    menu_btn.classList.add('open_icon');
+  }
+});
